8ba905711c47493ab2c1b25f423fb3fb086f250c
gasparluik <luik.gaspar@gmail.com>
2021-09-17 21:11:44 +0300

    assignment done
    hope it doesn't mess up the folder

---
+++ assignment1/java-backend/src/main/java/ee/gaspar/backend/controller/CategoryController.java
@@ -0,0 +1,44 @@
+package ee.gaspar.backend.controller;
+
+import ee.gaspar.backend.model.Category;
+import ee.gaspar.backend.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("category")
+    public List<Category> getCategories(){
+        return categoryService.getCategories();
+    }
+
+    @PostMapping("categories")
+    public String postCategory(@RequestBody Category category){
+        categoryService.saveCategory(category);
+        return "Kategooria " + category.getName() +" tüüp on  " + category.getCategory();
+        //Postmanis saab POST meetodit checkida. Kasuta RAW inputi ja URL-iks localhost:8080/items
+    }
+
+    //tee serverile restart
+    // localhost:8080/items
+
+    //delete päring
+    //edit päring
+    //view one item päring
+
+    //andmebaas
+
+    }
