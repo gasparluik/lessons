650620cdf0a2a85dc3940f7bdeeb7f29e2b08794
Gaspar Luik <gasparl@tlu.ee>
2021-11-02 13:24:29 +0200

    icon changed

--- 2021-10-25/frontend/src/components/Navbar.js
+++ 2021-10-25/frontend/src/components/Navbar.js
@@ -3,17 +3,32 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return(
-    <div className="navbar">
-      <Link to="/">
-        <img className="logo" src="/webshio.png" alt=""/>
-      </Link>
-      <Link to="/cart">
-        <img className="cart" src="/cart.svg" alt=""/>
-      </Link>
-    <Link to="/admin">
-        <button className="admin">Admini vaatesse</button>
-    </Link>
-    </div>
+      <div className="navbar">
+          <div className="logo">
+              <Link to="/">
+                  <img src="logo192.png" alt="logo"/>
+              </Link>
+          </div>
+
+          <div className="navbar-btns">
+              <Link to="add-item">
+                  <button>Lisa uus ese</button>
+              </Link>
+              <Link to="/">
+                  <button>Kategooriad</button>
+              </Link>
+              <Link to="/admin">
+                  <button>Admini vaatesse</button>
+              </Link>
+          </div>
+
+          <div className="cart">
+              <Link to="cart">
+                  <img src="cart.svg" alt=""/>
+              </Link>
+          </div>
+
+      </div>
   );
 }
 
